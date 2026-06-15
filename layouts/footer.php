8cc702bb326b7549abed4202d078c314c2db938d
mars22144 <fauziadnansptr@gmail.com>
2026-06-15 09:17:21 +0700

    add button back to top

--- layouts/footer.php
+++ layouts/footer.php
@@ -20,6 +20,7 @@
 </footer>
 
 <?php $base_url = '/peppy_bakery'; ?>
+<div class='wbnz-back-to-top-button' data-webynize-id='11216'></div><script defer src='https://widgets.webynize.com/back-to-top-button.js'></script>
 <script src="<?= $base_url ?>/assets/js/main.js"></script>
 </body>
 </html>
