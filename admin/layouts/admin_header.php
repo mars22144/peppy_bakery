2f75895da0299159cf7ded6f16c941d8beb5d6db
mars22144 <fauziadnansptr@gmail.com>
2026-06-21 20:26:35 +0700

    hps google font

--- admin/layouts/admin_header.php
+++ admin/layouts/admin_header.php
@@ -13,7 +13,6 @@ $base_url = '/peppy_bakery';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin - Peppy Bakery</title>
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=DM+Sans:wght@400;500;600&display=swap" rel="stylesheet"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
     <link rel="stylesheet" href="<?= $base_url ?>/assets/css/admin.css">
 </head>
